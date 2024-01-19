4ae8ebe308704a94d6c1f14b6a4d8eb3adcdc687
SimonaR07 <sristomanova32@gmail.com>
2024-01-19 20:27:59 +0100

    Add new java program

---
+++ src/Stepen.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Stepen {
+	
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		
+		System.out.println("Vnesi celziusovi stepeni: ");
+		double c = input.nextDouble();
+		
+		double f=9*c/5+32;
+		System.out.println("Celziusovi stepeni se: " +c );
+		System.out.println("Celziusovi stepeni vo Farenhajtovi se: " + f );
+		
+		
+
+	}
+
+}
+
+
